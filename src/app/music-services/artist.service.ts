05c10932468b1ab5d658dbd93e64c4f380cbd6ea
Alexandros Theofilis <Alexandros.Theofilis@gr.ey.com>
2018-10-17 17:38:22 +0300

    move style to .css

--- src/app/music-services/artist.service.ts
+++ src/app/music-services/artist.service.ts
@@ -11,7 +11,7 @@ export class ArtistService {
   private api_key = '4e53aea22e9246268b35c73708b470e6';
 
   // tslint:disable-next-line:max-line-length
-  private token = 'BQCK5A-ttXSYAe_N7Un5S2U16cYloNx-uizNZYWCXyVkEEkdyvYaHHcpJFxwRBh0Ppt5cJkWCKxgCnaY2EgANB-gEpk7sBbZxvtSjXSp7GOHZTcSe9t9djZjmDNLyGNAanihKtrr4sPvAbQuiri4fLLjYOvXsXJCU_r9L4M-EmrQZRnBjto';
+  private token = 'BQDCeOu8rWC4r5TgiDk_rEgj7lxF14Jajd7vui7UhV0uVGwTcn8t2EM9z8LzLym3j7zke7BPK6e5dhp-UOlq0ubjHYnFbgTed4Wyq10ENZbCxI7rK6xQ8M66oQgekGGHXqQP3Tswtf_afkE_9Tej1QBkRs3PjBdt-EtxhOT6qMI4tSO7-O8';
 
   private spotifyHeader = new HttpHeaders(
     {'Content-Type': 'application/json',
