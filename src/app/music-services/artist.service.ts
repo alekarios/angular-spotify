91ff1c52bd24a40aae6ef902a1c53e42d926c1dd
Alexandros Theofilis <Alexandros.Theofilis@gr.ey.com>
2018-10-18 10:37:17 +0300

    change token

--- src/app/music-services/artist.service.ts
+++ src/app/music-services/artist.service.ts
@@ -11,7 +11,7 @@ export class ArtistService {
   private api_key = '4e53aea22e9246268b35c73708b470e6';
 
   // tslint:disable-next-line:max-line-length
-  private token = 'BQDCeOu8rWC4r5TgiDk_rEgj7lxF14Jajd7vui7UhV0uVGwTcn8t2EM9z8LzLym3j7zke7BPK6e5dhp-UOlq0ubjHYnFbgTed4Wyq10ENZbCxI7rK6xQ8M66oQgekGGHXqQP3Tswtf_afkE_9Tej1QBkRs3PjBdt-EtxhOT6qMI4tSO7-O8';
+  private token = 'BQC1kYbyGw8HKfnDPp9hAztcgLcBjhTWVKu8yzbsAUyQlcIW0MDx60X2wlB_RgXOw9uAfPzt1JoP4FrCJIk4B5M3ctTUwyDQXlJA2L_AqbaQGcC95epz5XKrCi_7NFcOu8x7DyRlaP5AakoclipBGJj5CMYz2RHmAQLlfbxR3d8qwPd7HK4';
 
   private spotifyHeader = new HttpHeaders(
     {'Content-Type': 'application/json',
